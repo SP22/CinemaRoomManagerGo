b8ad006296e0ea0815fcb4185bffb9cc1dcd7379
ihor shyshov <sp.ukr.rs@gmail.com>
2025-03-03 00:09:50 +0200

    Added ticket purchase

--- Cinema Room Manager (Go)/task/main.go
+++ Cinema Room Manager (Go)/task/main.go
@@ -2,25 +2,63 @@ package main
 
 import "fmt"
 
+func printSeating(cinema [][]rune) {
+	fmt.Println("Cinema:")
+	fmt.Print(" ")
+	for i := 1; i <= len(cinema[0]); i++ {
+		fmt.Printf(" %d", i)
+	}
+	fmt.Println()
+	for i, row := range cinema {
+		fmt.Printf("%d", i+1)
+		for _, seat := range row {
+			fmt.Printf(" %c", seat)
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
-	rows := 7
-	cols := 8
+	var rows, seats int
 
 	fmt.Println("Enter the number of rows:")
 	fmt.Scan(&rows)
 	fmt.Println("Enter the number of seats in each row:")
-	fmt.Scan(&cols)
+	fmt.Scan(&seats)
 
-	seats := rows * cols
-	income := 0
+	cinema := make([][]rune, rows)
+	for i := range cinema {
+		cinema[i] = make([]rune, seats)
+		for j := range cinema[i] {
+			cinema[i][j] = 'S'
+		}
+	}
 
-	if seats <= 60 {
-		income = seats * 10
+	printSeating(cinema)
+
+	// Get seat selection
+	var rowNumber, seatNumber int
+	fmt.Println("Enter a row number:")
+	fmt.Scan(&rowNumber)
+	fmt.Println("Enter a seat number in that row:")
+	fmt.Scan(&seatNumber)
+
+	totalSeats := rows * seats
+	var ticketPrice int
+
+	if totalSeats <= 60 {
+		ticketPrice = 10
 	} else {
 		firstHalf := rows / 2
-		secondHalf := rows - firstHalf
-		income = cols * (firstHalf*10 + secondHalf*8)
+		if rowNumber <= firstHalf {
+			ticketPrice = 10
+		} else {
+			ticketPrice = 8
+		}
 	}
-	fmt.Println("Total income:\n")
-	fmt.Printf("$%d", income)
+	fmt.Printf("Ticket price: $%d\n", ticketPrice)
+
+	cinema[rowNumber-1][seatNumber-1] = 'B'
+
+	printSeating(cinema)
 }
